c58aee84c17071dab402396d0f829ec6e4ec378c
Nanokaso Gongon <natalygoncalves1997@gmail.com>
2020-11-09 20:29:31 -0300

    a

--- Sistema/Sistema/Login.Designer.cs
+++ Sistema/Sistema/Login.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             this.pnlGeral = new System.Windows.Forms.Panel();
-            this.lblCadastrar = new System.Windows.Forms.Label();
-            this.btnContinuar = new System.Windows.Forms.Button();
             this.btnUpload = new System.Windows.Forms.Button();
             this.picImage = new System.Windows.Forms.PictureBox();
+            this.lblCadastrar = new System.Windows.Forms.Label();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlGeral.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlGeral
             // 
+            this.pnlGeral.Controls.Add(this.label1);
             this.pnlGeral.Controls.Add(this.btnUpload);
             this.pnlGeral.Controls.Add(this.picImage);
             this.pnlGeral.Controls.Add(this.lblCadastrar);
@@ -49,36 +51,20 @@
             this.pnlGeral.Size = new System.Drawing.Size(464, 441);
             this.pnlGeral.TabIndex = 1;
             // 
-            // lblCadastrar
-            // 
-            this.lblCadastrar.AutoSize = true;
-            this.lblCadastrar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblCadastrar.Location = new System.Drawing.Point(14, 409);
-            this.lblCadastrar.Name = "lblCadastrar";
-            this.lblCadastrar.Size = new System.Drawing.Size(116, 13);
-            this.lblCadastrar.TabIndex = 2;
-            this.lblCadastrar.Text = "Cadastrar novo usuario";
-            this.lblCadastrar.Click += new System.EventHandler(this.lblCadastrar_Click);
-            // 
-            // btnContinuar
-            // 
-            this.btnContinuar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnContinuar.Location = new System.Drawing.Point(340, 378);
-            this.btnContinuar.Name = "btnContinuar";
-            this.btnContinuar.Size = new System.Drawing.Size(107, 44);
-            this.btnContinuar.TabIndex = 1;
-            this.btnContinuar.Text = "Continuar";
-            this.btnContinuar.UseVisualStyleBackColor = true;
-            // 
             // btnUpload
             // 
+            this.btnUpload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
             this.btnUpload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.ForeColor = System.Drawing.Color.Cyan;
             this.btnUpload.Location = new System.Drawing.Point(172, 275);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(107, 44);
             this.btnUpload.TabIndex = 5;
             this.btnUpload.Text = "Upload";
-            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // picImage
             // 
@@ -86,19 +72,65 @@
             this.picImage.Location = new System.Drawing.Point(123, 66);
             this.picImage.Name = "picImage";
             this.picImage.Size = new System.Drawing.Size(200, 200);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picImage.TabIndex = 4;
             this.picImage.TabStop = false;
+            // 
+            // lblCadastrar
+            // 
+            this.lblCadastrar.AutoSize = true;
+            this.lblCadastrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastrar.ForeColor = System.Drawing.Color.Cyan;
+            this.lblCadastrar.Location = new System.Drawing.Point(14, 409);
+            this.lblCadastrar.Name = "lblCadastrar";
+            this.lblCadastrar.Size = new System.Drawing.Size(163, 18);
+            this.lblCadastrar.TabIndex = 2;
+            this.lblCadastrar.Text = "Cadastrar novo usuario";
+            this.lblCadastrar.Click += new System.EventHandler(this.lblCadastrar_Click);
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnContinuar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinuar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinuar.ForeColor = System.Drawing.Color.Cyan;
+            this.btnContinuar.Location = new System.Drawing.Point(340, 378);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(107, 44);
+            this.btnContinuar.TabIndex = 1;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = false;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(66, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Logar";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(484, 461);
             this.Controls.Add(this.pnlGeral);
             this.Name = "Login";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
+            this.Load += new System.EventHandler(this.Login_Load);
             this.pnlGeral.ResumeLayout(false);
             this.pnlGeral.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
@@ -113,5 +145,6 @@
         private System.Windows.Forms.Button btnContinuar;
         private System.Windows.Forms.Button btnUpload;
         private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Sistema/Sistema/Login.cs
+++ Sistema/Sistema/Login.cs
@@ -15,13 +15,83 @@ namespace Sistema
         public Login()
         {
             InitializeComponent();
+            btnContinuar.Enabled = false;
         }
 
+        public Login(string caminho)
+        {
+            InitializeComponent();
+            carregarCaminho(caminho);
+        }
+
+        string caminho { get; set; }
+        bool isClickButton { get; set; }
         private void lblCadastrar_Click(object sender, EventArgs e)
         {
             Cadastro c = new Cadastro();
             c.Show();
-            this.Close();
+            isClickButton = true;
+            Close();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        { }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            btnContinuar.Enabled = false;
+            try
+            {
+                var caminho = UsuarioDAO.capturarImagem();
+
+                carregarCaminho(caminho);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void carregarCaminho(string caminho)
+        {
+            if (!string.IsNullOrWhiteSpace(caminho))
+            {
+                picImage.Image = Image.FromFile(caminho);
+                btnContinuar.Enabled = true;
+                this.caminho = caminho;
+            }
+        }
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(caminho))
+                {
+                    var u = UsuarioDAO.Logar(caminho);
+                    if (u != null)
+                    {
+                        Home h = new Home(u);
+                        h.Show();
+                        isClickButton = true;
+                        Close();
+                    }
+                }
+                else
+                {
+                    throw new Exception("Escolha uma imagem para continuar!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Login_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!isClickButton)
+            {
+                Application.Exit();
+            }
         }
     }
 }
