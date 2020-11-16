df90b59324c7ab6cccc2cc05192903b0a1b1252a
Nanokaso Gongon <natalygoncalves1997@gmail.com>
2020-11-15 22:37:04 -0300

    update

--- Sistema/Sistema/UsuarioTO.cs
+++ Sistema/Sistema/UsuarioTO.cs
@@ -10,5 +10,20 @@ namespace Sistema
     {
         public string Nome { get; set; }
         public int Perfil { get; set; }
+
+        public string PerfilNome
+        {
+            get
+            {
+                string tipo = "";
+                if (Perfil == 1)
+                { tipo = "Funcionario(a)"; }
+                if (Perfil == 2)
+                { tipo = "Diretor(a)"; }
+                if (Perfil == 3)
+                { tipo = "Ministro(a)"; }
+                return tipo;
+            }
+        }
     }
 }
